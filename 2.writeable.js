ed0fccf605e8773d6e9de02f74a686063680e4eb
dearzoe <2421113739@qq.com>
2016-10-31 01:15:49 +0800

    可写流

--- 2.writeable.js
+++ 2.writeable.js
@@ -1,4 +1,21 @@
 var fs = require("fs");
-var ws = fs.createWriteStream("2.tet",{
-   flags:
+var ws = fs.createWriteStream("2.txt",{
+   flags:"w",
+   start:1,
+   highWaterMark:3
 });
+var i=0;
+var max = 10;
+function write(){
+   console.log("继续写"+i);
+   var flag=true;
+   while(flag && i<max){
+      flag=ws.write(String(i++))
+   }
+}
+write();
+ws.on("drain", function () {
+   console.log("写满了"+i);
+   write()
+});
+
